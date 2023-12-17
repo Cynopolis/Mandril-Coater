8be84f60ca01d1f931bd7f399a06496ca504b1d7
Quinn <henth013@d.umn.edu>
2023-12-17 13:00:10 -0500

    Fixed paltformio.ini config.

--- src/main.cpp
+++ src/main.cpp
@@ -2,24 +2,27 @@
 #include "MACHINE-PARAMETERS.h"
 
 #include <Arduino.h>
-#include "StepperMotor.h"
+// #include "StepperMotor.h"
 
 
-StepperMotor linearMotor(LINEAR_MOTOR_STEP_PIN, LINEAR_MOTOR_DIRECTION_PIN, LINEAR_MOTOR_ENABLE_PIN, STEPS_PER_MM);
-StepperMotor rotationMotor(ROTATION_MOTOR_STEP_PIN, ROTATION_MOTOR_DIRECTION_PIN, ROTATION_MOTOR_ENABLE_PIN, STEPS_PER_REVOLUTION);
+// StepperMotor linearMotor(LINEAR_MOTOR_STEP_PIN, LINEAR_MOTOR_DIRECTION_PIN, LINEAR_MOTOR_ENABLE_PIN, STEPS_PER_MM);
+// StepperMotor rotationMotor(ROTATION_MOTOR_STEP_PIN, ROTATION_MOTOR_DIRECTION_PIN, ROTATION_MOTOR_ENABLE_PIN, STEPS_PER_REVOLUTION);
 
 void setup() {
   Serial.begin(SERIAL_BAUD_RATE);
   Serial.println("Beginning Machine Setup");
   
   // motor setup
-  linearMotor.Init();
-  linearMotor.setMaximums(LINEAR_MOTOR_MAX_SPEED_MM_PER_MIN, LINEAR_MOTOR_MAX_ACCELERATION_MM_PER_MIN_PER_MIN);
+  // linearMotor.Init();
+  // linearMotor.setMaximums(LINEAR_MOTOR_MAX_SPEED_MM_PER_MIN, LINEAR_MOTOR_MAX_ACCELERATION_MM_PER_MIN_PER_MIN);
   
-  rotationMotor.Init();
-  rotationMotor.setMaximums(ROTATION_MOTOR_MAX_SPEED_DEGREES_PER_MIN, ROTATION_MOTOR_MAX_ACCELERATION_DEGREES_PER_MIN_PER_MIN);
+  // rotationMotor.Init();
+  // rotationMotor.setMaximums(ROTATION_MOTOR_MAX_SPEED_DEGREES_PER_MIN, ROTATION_MOTOR_MAX_ACCELERATION_DEGREES_PER_MIN_PER_MIN);
+
+  Serial.println("Finished Machine Setup");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  Serial.println("Hello World");
 }
