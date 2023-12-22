01f303ea05f9723c395e6f4065cc001eebda01a4
Quinn <henth013@d.umn.edu>
2023-12-22 17:57:28 -0500

    Changed linear motor definitions.

--- include/PINOUT.h
+++ include/PINOUT.h
@@ -1,8 +1,8 @@
 // <------stepper motor pin definitions------->
 // linear definitions
-#define LINEAR_MOTOR_STEP_PIN 2
-#define LINEAR_MOTOR_DIRECTION_PIN 3
-#define LINEAR_MOTOR_ENABLE_PIN 4
+#define LINEAR_MOTOR_STEP_PIN 36
+#define LINEAR_MOTOR_DIRECTION_PIN 37
+#define LINEAR_MOTOR_ENABLE_PIN 38
 
 // rotation definitions
 #define ROTATION_MOTOR_STEP_PIN 5
